99de77cb583dfded908d1348c2ab48612cfbcd31
rpss2 <rpss@cin.ufpe.br>
2018-05-20 18:21:15 -0300

    Starting methods implementations

--- AST MiniJava/04-MiniJavaAST/src/br/ufpe/cin/if688/minijava/main/ASTVisitor.java
+++ AST MiniJava/04-MiniJavaAST/src/br/ufpe/cin/if688/minijava/main/ASTVisitor.java
@@ -5,6 +5,29 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.RuleNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.IntArrayData;
+
+import br.ufpe.cin.if688.minijava.ast.BooleanType;
+import br.ufpe.cin.if688.minijava.ast.ClassDecl;
+import br.ufpe.cin.if688.minijava.ast.ClassDeclExtends;
+import br.ufpe.cin.if688.minijava.ast.ClassDeclList;
+import br.ufpe.cin.if688.minijava.ast.ClassDeclSimple;
+import br.ufpe.cin.if688.minijava.ast.Exp;
+import br.ufpe.cin.if688.minijava.ast.Formal;
+import br.ufpe.cin.if688.minijava.ast.FormalList;
+import br.ufpe.cin.if688.minijava.ast.Identifier;
+import br.ufpe.cin.if688.minijava.ast.IdentifierType;
+import br.ufpe.cin.if688.minijava.ast.IntArrayType;
+import br.ufpe.cin.if688.minijava.ast.IntegerType;
+import br.ufpe.cin.if688.minijava.ast.MainClass;
+import br.ufpe.cin.if688.minijava.ast.MethodDecl;
+import br.ufpe.cin.if688.minijava.ast.MethodDeclList;
+import br.ufpe.cin.if688.minijava.ast.Program;
+import br.ufpe.cin.if688.minijava.ast.Statement;
+import br.ufpe.cin.if688.minijava.ast.StatementList;
+import br.ufpe.cin.if688.minijava.ast.Type;
+import br.ufpe.cin.if688.minijava.ast.VarDecl;
+import br.ufpe.cin.if688.minijava.ast.VarDeclList;
 import br.ufpe.cin.if688.minijava.main.AntlrParser.ClassDeclarationContext;
 import br.ufpe.cin.if688.minijava.main.AntlrParser.ExpressionContext;
 import br.ufpe.cin.if688.minijava.main.AntlrParser.GoalContext;
@@ -43,20 +66,50 @@ public class ASTVisitor implements AntlrVisitor<Object> {
 
 	@Override
 	public Object visitIdentifier(IdentifierContext ctx) {
-		// TODO Auto-generated method stub
-		return null;
+		Identifier id = new Identifier(ctx.getText());
+		return id;
 	}
 
 	@Override
 	public Object visitMethodDeclaration(MethodDeclarationContext ctx) {
-		// TODO Auto-generated method stub
-		return null;
+		Type at = (Type) ctx.type(0).accept(this);
+		Identifier ai = (Identifier) ctx.identifier(0).accept(this);
+		
+		FormalList afl = new FormalList();
+		for (int i = 1; i < ctx.type().size(); i++) {
+			Type t = (Type) ctx.type(i).accept(this);
+			Identifier id = (Identifier) ctx.identifier(i).accept(this);
+			Formal formal = new Formal(t, id);
+			afl.addElement(formal);
+		}
+		
+		VarDeclList avl = new VarDeclList();
+		for (VarDeclarationContext vdc : ctx.varDeclaration()) {
+			avl.addElement((VarDecl) vdc.accept(this));
+		}
+		
+		StatementList asl = new StatementList();
+		for (StatementContext sc : ctx.statement()) {
+			asl.addElement((Statement) sc.accept(this));
+		}
+		
+		Exp ae = (Exp) ctx.expression().accept(this);
+		
+		MethodDecl md = new MethodDecl(at, ai, afl, avl, asl, ae);
+		return md;
 	}
 
 	@Override
 	public Object visitGoal(GoalContext ctx) {
-		// TODO Auto-generated method stub
-		return null;
+		MainClass am = (MainClass) ctx.mainClass().accept(this);
+		
+		ClassDeclList acl = new ClassDeclList();
+		for(ClassDeclarationContext cdc : ctx.classDeclaration()) {
+			acl.addElement((ClassDecl) cdc.accept(this));
+		}
+		
+		Program p = new Program(am, acl);
+		return p;
 	}
 
 	@Override
@@ -67,8 +120,12 @@ public class ASTVisitor implements AntlrVisitor<Object> {
 
 	@Override
 	public Object visitMainClass(MainClassContext ctx) {
-		// TODO Auto-generated method stub
-		return null;
+		Identifier ai1 = (Identifier) ctx.identifier(0).accept(this);
+		Identifier ai2 = (Identifier) ctx.identifier(1).accept(this);
+		Statement as = (Statement) ctx.statement().accept(this);
+		
+		MainClass mc = new MainClass(ai1, ai2, as);
+		return mc;
 	}
 
 	@Override
@@ -79,20 +136,50 @@ public class ASTVisitor implements AntlrVisitor<Object> {
 
 	@Override
 	public Object visitType(TypeContext ctx) {
-		// TODO Auto-generated method stub
-		return null;
+		String type = ctx.getText();
+		switch (type) {
+		case "int":
+			return new IntegerType();
+		case "boolean":
+			return new BooleanType();
+		case "int []":
+			return new IntArrayType();
+		default:
+			return new IdentifierType(type);
+		}
 	}
 
 	@Override
 	public Object visitVarDeclaration(VarDeclarationContext ctx) {
-		// TODO Auto-generated method stub
-		return null;
+		Type at = (Type) ctx.type().accept(this);
+		Identifier ai = (Identifier) ctx.identifier().accept(this);
+		
+		VarDecl vd = new VarDecl(at, ai);
+		return vd;
 	}
 
 	@Override
 	public Object visitClassDeclaration(ClassDeclarationContext ctx) {
-		// TODO Auto-generated method stub
-		return null;
+		ClassDecl cd = null;
+		
+		int sizeList = ctx.identifier().size();
+		
+		VarDeclList vdl = new VarDeclList();
+		for(VarDeclarationContext vdc : ctx.varDeclaration()) {
+			vdl.addElement((VarDecl) vdc.accept(this));
+		}
+		
+		MethodDeclList mdl = new MethodDeclList();
+		for(MethodDeclarationContext mdc : ctx.methodDeclaration()) {
+			mdl.addElement((MethodDecl) mdc.accept(this));
+		}
+		
+		if(sizeList > 1) {
+			cd = new ClassDeclExtends((Identifier) ctx.identifier(0).accept(this), (Identifier) ctx.identifier(1).accept(this), vdl, mdl);
+		} else {
+			cd = new ClassDeclSimple((Identifier) ctx.identifier(0).accept(this), vdl, mdl);
+		}
+		return cd;
 	}
 
 }
--- AST MiniJava/04-MiniJavaAST/src/br/ufpe/cin/if688/minijava/main/Main.java
+++ AST MiniJava/04-MiniJavaAST/src/br/ufpe/cin/if688/minijava/main/Main.java
@@ -1,40 +1,30 @@
 package br.ufpe.cin.if688.minijava.main;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
-import br.ufpe.cin.if688.minijava.ast.BooleanType;
-import br.ufpe.cin.if688.minijava.ast.ClassDeclExtends;
-import br.ufpe.cin.if688.minijava.ast.ClassDeclList;
-import br.ufpe.cin.if688.minijava.ast.ClassDeclSimple;
-import br.ufpe.cin.if688.minijava.ast.Identifier;
-import br.ufpe.cin.if688.minijava.ast.IdentifierType;
-import br.ufpe.cin.if688.minijava.ast.IntegerLiteral;
-import br.ufpe.cin.if688.minijava.ast.IntegerType;
-import br.ufpe.cin.if688.minijava.ast.MainClass;
-import br.ufpe.cin.if688.minijava.ast.MethodDeclList;
-import br.ufpe.cin.if688.minijava.ast.Print;
 import br.ufpe.cin.if688.minijava.ast.Program;
-import br.ufpe.cin.if688.minijava.ast.VarDecl;
-import br.ufpe.cin.if688.minijava.ast.VarDeclList;
 import br.ufpe.cin.if688.minijava.visitor.PrettyPrintVisitor;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		InputStream stream = new FileInputStream("input.txt");
 		ANTLRInputStream input = new ANTLRInputStream(stream);
 		AntlrLexer lexer = new AntlrLexer(input);
 		CommonTokenStream token = new CommonTokenStream(lexer);
 		AntlrParser parser = new AntlrParser(token);	
 
-		Program p = new Program(main, cdl);
+		Program prog = (Program) new ASTVisitor().visit(parser.goal());
 		
-		PrettyPrintVisitor ppv = new PrettyPrintVisitor();
-		ppv.visit(p);
+		//PrettyPrintVisitor ppv = new PrettyPrintVisitor();
+		//ppv.visit(prog);
+		
+		prog.accept(new PrettyPrintVisitor());
 	}
 
 }
