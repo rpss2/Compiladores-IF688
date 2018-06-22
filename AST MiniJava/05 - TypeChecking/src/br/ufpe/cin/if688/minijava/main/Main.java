c1ab19e9246b12560f80e70a0819ab9657e58983
rpss2 <rpss@cin.ufpe.br>
2018-06-22 15:37:03 -0300

    details main

--- AST MiniJava/05 - TypeChecking/src/br/ufpe/cin/if688/minijava/main/Main.java
+++ AST MiniJava/05 - TypeChecking/src/br/ufpe/cin/if688/minijava/main/Main.java
@@ -4,14 +4,19 @@ import br.ufpe.cin.if688.minijava.ast.BooleanType;
 import br.ufpe.cin.if688.minijava.ast.ClassDeclExtends;
 import br.ufpe.cin.if688.minijava.ast.ClassDeclList;
 import br.ufpe.cin.if688.minijava.ast.ClassDeclSimple;
+import br.ufpe.cin.if688.minijava.ast.Formal;
+import br.ufpe.cin.if688.minijava.ast.FormalList;
 import br.ufpe.cin.if688.minijava.ast.Identifier;
+import br.ufpe.cin.if688.minijava.ast.IdentifierExp;
 import br.ufpe.cin.if688.minijava.ast.IdentifierType;
 import br.ufpe.cin.if688.minijava.ast.IntegerLiteral;
 import br.ufpe.cin.if688.minijava.ast.IntegerType;
 import br.ufpe.cin.if688.minijava.ast.MainClass;
+import br.ufpe.cin.if688.minijava.ast.MethodDecl;
 import br.ufpe.cin.if688.minijava.ast.MethodDeclList;
 import br.ufpe.cin.if688.minijava.ast.Print;
 import br.ufpe.cin.if688.minijava.ast.Program;
+import br.ufpe.cin.if688.minijava.ast.StatementList;
 import br.ufpe.cin.if688.minijava.ast.VarDecl;
 import br.ufpe.cin.if688.minijava.ast.VarDeclList;
 import br.ufpe.cin.if688.minijava.visitor.BuildSymbolTableVisitor;
@@ -26,7 +31,6 @@ public class Main {
 				new Identifier("Testando"), 
 				new Print(new IntegerLiteral(2))
 		);
-		
 		VarDeclList vdl1 = new VarDeclList();
 		vdl1.addElement(new VarDecl(
 			new BooleanType(),
@@ -37,7 +41,45 @@ public class Main {
 				new Identifier("num")
 		));
 		
+		FormalList fl = new FormalList();
+		fl.addElement(new Formal(
+				new IntegerType(), 
+				new Identifier("x")
+		));
+		fl.addElement(new Formal(
+				new IntegerType(), 
+				new Identifier("y")
+		));
+		
+		VarDeclList vdlm1 = new VarDeclList();
+		vdlm1.addElement(new VarDecl(
+			new BooleanType(),
+			new Identifier("run")
+		));
+		vdlm1.addElement(new VarDecl(
+				new IntegerType(),
+				new Identifier("count")
+		));
+		
+
 		MethodDeclList mdl = new MethodDeclList();
+		mdl.addElement( new MethodDecl(
+				new IntegerType(),
+				new Identifier("func"),
+				fl,
+				vdlm1,
+				new StatementList(),
+				new IdentifierExp("count")
+		));
+	
+		mdl.addElement( new MethodDecl(
+				new IntegerType(),
+				new Identifier("func2"),
+				new FormalList(),
+				new VarDeclList(),
+				new StatementList(),
+				new IntegerLiteral(0)
+		));
 		
 		ClassDeclSimple A = new ClassDeclSimple(
 					new Identifier("A"), vdl1, mdl
