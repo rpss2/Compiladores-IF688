bf9b29a28428364910da5419efd8d74289903aae
rpss2 <rpss@cin.ufpe.br>
2018-06-21 23:33:07 -0300

    clean the project

--- AST MiniJava/05 - TypeChecking/src/br/ufpe/cin/if688/minijava/main/Main.java
+++ AST MiniJava/05 - TypeChecking/src/br/ufpe/cin/if688/minijava/main/Main.java
@@ -1,32 +1,76 @@
 package br.ufpe.cin.if688.minijava.main;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CommonTokenStream;
-
-import antlr.AntlrLexer;
-import antlr.AntlrParser;
+import br.ufpe.cin.if688.minijava.ast.BooleanType;
+import br.ufpe.cin.if688.minijava.ast.ClassDeclExtends;
+import br.ufpe.cin.if688.minijava.ast.ClassDeclList;
+import br.ufpe.cin.if688.minijava.ast.ClassDeclSimple;
+import br.ufpe.cin.if688.minijava.ast.Identifier;
+import br.ufpe.cin.if688.minijava.ast.IdentifierType;
+import br.ufpe.cin.if688.minijava.ast.IntegerLiteral;
+import br.ufpe.cin.if688.minijava.ast.IntegerType;
+import br.ufpe.cin.if688.minijava.ast.MainClass;
+import br.ufpe.cin.if688.minijava.ast.MethodDeclList;
+import br.ufpe.cin.if688.minijava.ast.Print;
 import br.ufpe.cin.if688.minijava.ast.Program;
+import br.ufpe.cin.if688.minijava.ast.VarDecl;
+import br.ufpe.cin.if688.minijava.ast.VarDeclList;
+import br.ufpe.cin.if688.minijava.visitor.BuildSymbolTableVisitor;
 import br.ufpe.cin.if688.minijava.visitor.PrettyPrintVisitor;
+import br.ufpe.cin.if688.minijava.visitor.TypeCheckVisitor;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		InputStream stream = new FileInputStream("src/test/resources/LinkedList.java");
-		ANTLRInputStream input = new ANTLRInputStream(stream);
-		AntlrLexer lexer = new AntlrLexer(input);
-		CommonTokenStream token = new CommonTokenStream(lexer);
+	public static void main(String[] args) {
+		MainClass main = new MainClass(
+				new Identifier("Teste"), 
+				new Identifier("Testando"), 
+				new Print(new IntegerLiteral(2))
+		);
 		
-		AntlrParser parser = new AntlrParser(token);	
+		VarDeclList vdl1 = new VarDeclList();
+		vdl1.addElement(new VarDecl(
+			new BooleanType(),
+			new Identifier("flag")
+		));
+		vdl1.addElement(new VarDecl(
+				new IntegerType(),
+				new Identifier("num")
+		));
+		
+		MethodDeclList mdl = new MethodDeclList();
+		
+		ClassDeclSimple A = new ClassDeclSimple(
+					new Identifier("A"), vdl1, mdl
+		);
+		
+		ClassDeclExtends B = new ClassDeclExtends(
+				new Identifier("B"), new Identifier("A"), 
+				new VarDeclList(), new MethodDeclList()
+		);
+		
+		VarDeclList vdl2 = new VarDeclList();
+		vdl2.addElement(new VarDecl(
+				new IdentifierType("A"),
+				new Identifier("obj")
+		));
+		ClassDeclSimple C = new ClassDeclSimple(
+				new Identifier("C"), vdl2, new MethodDeclList()
+		);
+		
+		ClassDeclList cdl = new ClassDeclList();
+		cdl.addElement(A);
+		cdl.addElement(B);
+		cdl.addElement(C);
 
-		Program prog = (Program) new ASTVisitor().visit(parser.goal());
-		//PrettyPrintVisitor ppv = new PrettyPrintVisitor();
-		//ppv.visit(prog);
+		Program p = new Program(main, cdl);
 		
-		prog.accept(new PrettyPrintVisitor());
+		PrettyPrintVisitor ppv = new PrettyPrintVisitor();
+		BuildSymbolTableVisitor stVis = new BuildSymbolTableVisitor();
+		stVis.visit(p);
+		TypeCheckVisitor tcv = new TypeCheckVisitor(stVis.getSymbolTable());
+		tcv.visit(p);
+
+		ppv.visit(p);
 	}
 
 }
